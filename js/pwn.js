d6ace7ea0bfcd1f8336960412168a4bfb912ee40
Lawin <56766256+Lawin0129@users.noreply.github.com>
2023-11-29 16:53:24 +0000

    Modify bootstrap url in payload

--- js/pwn.js
+++ js/pwn.js
@@ -250,6 +250,7 @@ function go() {
                 if (req.responseType != "arraybuffer") throw "y u no blob";
                 var arrayBuf = new Uint8Array(req.response);
                 var header = b2u32(arrayBuf.slice(0, 4));
+                console.log(header);
                 if (header != 4277009103) {
                     fail("header is invalid: " + hexit(header) + ", should be 0xfeedfacf\nwtf is your payload??");
                     return
